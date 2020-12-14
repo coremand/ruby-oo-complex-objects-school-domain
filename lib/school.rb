0733ab9d184b075c332bf02d7fee95a810e5aa85
coremand <valinevite@gmail.com>
2020-12-14 09:57:57 -0700

    commit tree c4120fd7ddd1abdfff951d89294601c1741ad559

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
 # code here!
+class School
+    attr_accessor :roster
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(name,grade)
+        if @roster[grade]
+            @roster[grade] << name
+        else
+            @roster[grade] = []
+            @roster[grade] << name
+            @roster
+        end
+    end
+    def grade(grade)
+        @roster[grade]
+    end
+    def sort
+        @roster.map{|key,value|[key,value.sort]}.sort.to_h
+    end
+end
